e4f364e7f317283ce708fbfc461b2016ce6a0a43
ctalk <rk3314042@gmail.com>
2021-02-09 05:06:03 -0700

    CastingDataTypes class

---
+++ CastingDataTypes.java
@@ -0,0 +1,16 @@
+
+public class CastingDataTypes {
+	public static void main (String[] args) {
+		// initial values
+		int i = 200;
+		// cast to short
+		short s = (short)i;
+		System.out.println (s);
+		
+		double d = (double)i;
+		System.out.println (d);
+		
+		byte b = (byte)i;
+		System.out.println (b);
+	}
+}
