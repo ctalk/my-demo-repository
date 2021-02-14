fb7990d86dff7a1f6bfe68f98d00bb1b71aa4e04
ctalk <rk3314042@gmail.com>
2021-02-13 17:27:14 -0700

    Added Lab-Overloading

---
+++ Lab-Overloading/src/OverloadedMethods.java
@@ -0,0 +1,22 @@
+
+public class OverloadedMethods {
+	public static void main (String[] args) {
+		OverloadedMethods om = new OverloadedMethods ();
+		om.print();
+		
+		om.print ("second print method");
+		om.print ("second", "third");
+	}
+	// overload methods here
+	public void print () {
+		System.out.println ("original print method");
+	}
+	
+	public void print (String str) {
+		System.out.println (str);
+	}
+	
+	public void print (String str, String str2) {
+		System.out.println (str + " " + str2);
+	}
+}
