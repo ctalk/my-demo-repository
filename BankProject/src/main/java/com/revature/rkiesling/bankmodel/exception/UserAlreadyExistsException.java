f6bc656a40d4418e37f2d97fe416b48f903e35e4
ctalk <rk3314042@gmail.com>
2021-02-28 02:36:06 -0700

    Added UserAlreadyExistsException

---
+++ BankProject/src/main/java/com/revature/rkiesling/bankmodel/exception/UserAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.revature.rkiesling.bankmodel.exception;
+
+import java.lang.Exception;
+
+@SuppressWarnings("serial")
+public class UserAlreadyExistsException extends Exception {
+	public UserAlreadyExistsException (String msg) {
+		super (msg);
+	}
+
+}
